0222444de5de649260ee873994ba738eb8b2454f
AbdullahTheScientist <abdullahmazri124@gmail.com>
2025-07-07 08:50:54 +0500

    uvicorn

--- app.py
+++ app.py
@@ -56,4 +56,5 @@ def create_resume(data: ResumeRequest):
 
 # For local development
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
